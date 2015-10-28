f9b173779df1b1403d12cd6fb3faf924a96512fc
sanjpradhan <sanjaykpradhan@gmail.com>
2015-10-28 11:00:55 +0000

    updated file structure

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Xively-SFDC-REST-API
-
+# Inspiration for this style of API is from articles by Jeff Douglas.  He has built lots of examples of using nforce into front end applications.  I have used his approach to build out my API using Node and Express in conjunction with nForce which is an NPM package
 
 ## Contributors
 * Sanjay Pradhan 
--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// register the hanglebars helpers
-require('./lib/hbsHelpers');
 // connect to salesforce org
 require('./lib/connection');
 
@@ -14,17 +12,10 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
@@ -35,33 +26,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-
-
-/*  SKP COMMENTED OUT TO TEST ERROR HANDING IN REST API AND  PROISE IMPLEMENTATION OF XIVELY POST REQUEST
-
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-*/
 
 module.exports = app;
